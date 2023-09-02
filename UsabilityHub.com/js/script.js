8c6faebae0670fcf8b4653c53c4b75900f9800ea
Umesh Raut <143353687+umeshraut2002@users.noreply.github.com>
2023-09-03 01:21:38 +0530

    Add files via upload
    UsabiltyHub.com 
      -- Simple Web Page Clone 
       --- of a landing Page By uSing Tech stack of HTML And CSS

---
+++ UsabilityHub.com/js/script.js
@@ -0,0 +1,9 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#mainfun'),
+//     smooth: true
+// });
+
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector(''),
+//     smooth: true
+// });
---
+++ UsabilityHub.com/readme.md
@@ -0,0 +1,11 @@
+For an Html--
+
+   -- Top banner
+   -- Nav Bars we use 
+      1. Product
+      2. custemer
+      3. price
+      4. Resources
+      5. Sign in buttons
+      6. Sign up buttons
+
